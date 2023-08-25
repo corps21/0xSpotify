ca6eac2fc6f20c637bdd45b2b38d0a760b4695f2
Swastik Kumar Majhi <spyderauto.1234@gmail.com>
2023-08-25 12:24:44 +0530

    data segregation

--- src/Data.sol
+++ src/Data.sol
@@ -5,9 +5,57 @@ pragma solidity 0.8.19;
 contract ListDB {
     address[] userList;
 
+    function getUserList() external view returns (address[] memory) {
+        return userList;
+    }
+
+    function setUserList(address _user) external {
+        userList.push(_user);
+    }
+
     mapping(address => bool) isUser;
 
-    mapping(address => UserInfo) userInfo;
+    function getIsUser(address _user) external view returns (bool) {
+        return isUser[_user];
+    }
+
+    function setIsUser(address _user, bool _toDeactivate) external {
+        isUser[_user] = !_toDeactivate;
+    }
+
+    mapping(address => UserInfo) userInfoLogs;
+
+    struct UserInfo {
+        string name;
+        address[] artistFollowed;
+        bytes32[] likedSongs;
+    }
+
+    //test for gas for other approaches
+    function setUserInfo(
+        string calldata _name,
+        address _artistFollowed,
+        bytes32 idOfSong,
+        bool duringRegistration,
+        address _user,
+        bool isLiked
+    ) external {
+        if (duringRegistration) {
+            userInfoLogs[_user].name = _name;
+        } else if (_artistFollowed != address(0)) {
+            userInfoLogs[_user].artistFollowed.push(_artistFollowed);
+        } else if (isLiked) {
+            userInfoLogs[_user].likedSongs.push(idOfSong);
+        }
+    }
+
+    struct Song {
+        string name;
+        Genre genreOfSong;
+    }
+
+    mapping(bytes32 => Song) idToSong;
+    mapping(bytes32 => address) fromIdToArtist;
 
     enum Genre {
         ROCK,
@@ -19,31 +67,45 @@ contract ListDB {
         POP
     }
 
-    struct UserInfo {
-        string name;
-        address[] artistFollowed;
-        Song[] likedSong;
-    }
-
-    struct Song {
-        string name;
-        string artist;
-        Genre genreOfSong;
-        bytes32 idOfSong;
-    }
-
-    mapping(bytes32 => Song) idToSong;
-
     address[] artistList;
+
+    function getArtistList() external view returns (address[] memory) {
+        return artistList;
+    }
+
+    function setArtistList(address _artist) external {
+        artistList.push(_artist);
+    }
 
     mapping(address => bool) isArtist;
 
-    mapping(address => ArtistInfo) artistInfo;
+    function getIsArtist(address _artist) external view returns(bool) {
+        return isArtist[_artist];
+    }
 
-    struct ArtistInfo {
-        string name;
-        Song[] songCreated;
+    function setIsArtist(address _artist ,bool toDeactivate) external {
+        isArtist[_artist] = !toDeactivate;
     }
 
 
+    mapping(address => ArtistInfo) artistInfoLogs;
+
+    function getArtistInfoLogs(address _artist) external view returns (ArtistInfo memory) {
+        return artistInfoLogs[_artist];
+    }
+
+    function setArtistInfoLogs(address _artist, string calldata _name, bytes32 codeToSong,bool duringRegistration) external{
+        if(duringRegistration) {
+            artistInfoLogs[_artist].name = _name;
+        }
+
+        else {
+            artistInfoLogs[_artist].songCreated.push(codeToSong);
+        }
+    }
+
+    struct ArtistInfo {
+        string name;
+        bytes32[] songCreated;
+    }
 }
