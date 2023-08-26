e8b5cd0bd6a9f31dddccf34964c6c12728d4f091
Swastik Kumar Majhi <spyderauto.1234@gmail.com>
2023-08-26 21:20:58 +0530

    Some Changes

--- script/Data.s.sol
+++ script/Data.s.sol
@@ -2,13 +2,14 @@
 
 pragma solidity 0.8.19;
 
-import {Script} from '../lib/forge-std/src/Script.sol';
+import {Script} from "../lib/forge-std/src/Script.sol";
 import {Data} from "../src/Data.sol";
 import {ArtistRegistration} from "../src/ArtistRegistration.sol";
 import {UserRegistration} from "../src/UserRegistration.sol";
 
 contract DataDeploy is Script {
     function setUp() public {}
+
     function run() public {
         vm.startBroadcast();
         Data data = new Data();
